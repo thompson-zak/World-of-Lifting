c5553d747e676e6d814baf47a35efac98a931733
thompson-zak <zakthompson97@gmail.com>
2022-12-04 13:43:30 -0600

    Include basic styling and form. Create basic POST endpoint.

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 
 
@@ -17,6 +17,25 @@ CORS(app, resources={r'/*': {'origins': '*'}})
 @app.route('/ping', methods=['GET'])
 def ping_pong():
     return jsonify('pong!')
+
+@app.route('/analyze-lifts', methods=['POST'])
+def analyze_lifts():
+    lift_data = request.get_json()
+
+    squat = lift_data.get('squat')
+    bench = lift_data.get('bench')
+    deadlift = lift_data.get('deadlift')
+    units = lift_data.get('units')
+
+    response = {
+        'status': 'Success',
+        'squat': squat,
+        'bench': bench,
+        'deadlift': deadlift,
+        'units': units
+    }
+
+    return jsonify(response)
 
 
 if __name__ == '__main__':
---
+++ frontend/src/components/HomePage.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-sm-10">
+        <h1>Compare Your Lifts!</h1>
+        <hr><br><br>
+        <form>
+          <label for="squat">Squat:
+            <input type="text" id="squat" name="squat">
+          </label>
+          <br>
+          <label for="bench">Bench:
+            <input type="text" id="bench" name="bench">
+          </label>
+          <br>
+          <label for="deadlift">Deadlift:
+            <input type="text" id="deadlift" name="deadlift">
+          </label>
+          <br><br>
+          <p>What units are your lifts in?</p>
+          <label for="lbs">lbs
+            <input type="radio" id="lbs" name="lift_units">
+          </label>
+          <br>
+          <label for="kgs">kgs
+            <input type="radio" id="kgs" name="lift_units">
+          </label>
+        </form>
+        <hr><br><br>
+        <button type="button" class="btn btn-success btn-sm">Analyze Lifts!</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  name: 'HomePage',
+  data() {
+    return {
+      msg: '',
+    };
+  },
+  methods: {
+    getMessage() {
+      const path = 'http://localhost:5000/ping';
+      axios.get(path)
+        .then((res) => {
+          this.msg = res.data;
+        })
+        .catch((error) => {
+          // eslint-disable-next-line
+          console.error(error);
+        });
+    },
+  },
+  created() {
+    this.getMessage();
+  },
+};
+</script>
