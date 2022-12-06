e0480faa6306db500def5d93e60c2a41580f5d89
thompson-zak <zakthompson97@gmail.com>
2022-12-05 18:02:20 -0600

    Added support for downloading PL data zip file

--- backend/populatePowerliftingTable.py
+++ backend/populatePowerliftingTable.py
@@ -6,12 +6,15 @@ from sqlalchemy import create_engine
 from sqlalchemy.sql import text
 import yaml
 import time
+import urllib.request
 
 config = yaml.safe_load(open("./config.yml"))
 print(config)
 
-# TODO - full download of the ZIP file from open powerlifting
-# Leave this until nearly done so as to avoid unnecessary downloads
+print('Starting download of open powerlifting data')
+startDownloadTime = time.time()
+urllib.request.urlretrieve('https://openpowerlifting.gitlab.io/opl-csv/files/openpowerlifting-latest.zip', '../backend/downloads/openpowerlifting-latest.zip')
+print('Completed download of open powerlifting data in %s seconds', round(time.time() - startDownloadTime, 2))
 
 zf = zipfile.ZipFile('../backend/downloads/openpowerlifting-latest.zip')
 for info in zf.infolist():
@@ -27,7 +30,7 @@ dateColumns=['Date']
 dataTypes={'Sex': 'string', 'Age': float, 'AgeClass': 'string', 'Best3SquatKg': float, 'Best3BenchKg': float, 'Best3DeadliftKg': float, 'Place': 'string', 'Federation': 'string'}
 
 engine = create_engine(config['database']['connection']['string'])
-startTime = time.time()
+startIngestionTime = time.time()
 
 try:
 	engine.connect().execution_options(autocommit=True).execute(text('TRUNCATE TABLE powerlifting'))
@@ -35,14 +38,13 @@ try:
 		for chunk in reader:
 			chunk.columns = [colName.lower() for colName in requiredColumns]
 			chunk.to_sql('powerlifting', engine, if_exists='append', index=False)
-	print('Ingestion complete. Finished in %s seconds', round(time.time() - startTime, 2))
+	print('Ingestion complete. Finished in %s seconds', round(time.time() - startIngestionTime, 2))
 except Exception as e:
 	print(e)
-	print('There was an issue. Ingestion did not complete successfully. Finished in %s seconds', round(time.time() - startTime, 2))
+	print('There was an issue. Ingestion did not complete successfully. Finished in %s seconds', round(time.time() - startIngestionTime, 2))
 
 print('Cleaning up downloaded and extracted files...')
 os.remove('../backend/downloads/openpowerlifting.csv')
-# TODO - add this back once a full download is being performed for every run of this script
-#os.remove('../backend/downloads/openpowerlifting-latest.zip')
+os.remove('../backend/downloads/openpowerlifting-latest.zip')
 print('Downloaded and extracted files have been deleted.')
 print('Weekly refresh has been completed.')
