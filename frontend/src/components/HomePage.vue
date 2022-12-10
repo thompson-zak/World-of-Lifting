c993036c4ab6e9aa0a6fe670c233efee33e04125
thompson-zak <zakthompson97@gmail.com>
2022-12-10 12:13:38 -0600

    Replaced BootstrapVue for BootstrapVue3. Basic HomePage form done.

--- frontend/src/components/HomePage.vue
+++ frontend/src/components/HomePage.vue
@@ -1,143 +1,139 @@
 <template>
-  <div class="container">
-    <div>
+  <b-card>
+    <div class="container">
+      <div>
 
-      <h1>Compare Your Lifts!</h1>
+        <h1>Compare Your Lifts!</h1>
 
-      <hr><br><br>
+        <hr><br><br>
 
-      <b-form @submit="onSubmit" @reset="onReset" v-if="show">
-        <b-form-group
-          id="squat-group"
-          label="Squat:"
-          label-for="squat-input"
-        >
-          <b-form-input
-            id="squat-input"
-            v-model="form.squat"
-            type="number"
-            placeholder="Enter squat here"
-            required
-          ></b-form-input>
-        </b-form-group>
-
-        <b-form-group
-          id="bench-group"
-          label="Bench:"
-          label-for="bench-input"
-        >
-          <b-form-input
-            id="bench-input"
-            v-model="form.bench"
-            type="number"
-            placeholder="Enter bench press here"
-            required
-          ></b-form-input>
-        </b-form-group>
-
-        <b-form-group
-          id="deadlift-group"
-          label="Deadlift:"
-          label-for="deadlift-input"
-        >
-          <b-form-input
-            id="deadlift-input"
-            v-model="form.deadlift"
-            type="number"
-            placeholder="Enter deadlift here"
-            required
-          ></b-form-input>
-        </b-form-group>
-
-        <b-form-group id="lift-units-group">
-          <b-form-checkbox-group
-            v-model="form.liftUnits"
-            id="lift-units-checkbox"
+        <b-form @submit="onSubmit">
+          <b-form-group
+            id="squat-group"
+            label="Squat:"
+            label-for="squat-input"
           >
-            <b-form-checkbox value="lbs">lbs</b-form-checkbox>
-            <b-form-checkbox value="kgs">kgs</b-form-checkbox>
-          </b-form-checkbox-group>
-        </b-form-group>
+            <b-form-input
+              id="squat-input"
+              v-model="form.squat"
+              type="number"
+              placeholder="Enter squat here"
+              required
+            ></b-form-input>
+          </b-form-group>
 
-        <b-form-group
-          id="age-group"
-          label="Age:"
-          label-for="age-input"
-        >
-          <b-form-input
-            id="age-input"
-            v-model="form.age"
-            type="number"
-            placeholder="Enter age here"
-            required
-          ></b-form-input>
-        </b-form-group>
-
-        <b-form-group
-          id="bodyweight-group"
-          label="Bodyweight:"
-          label-for="bodyweight-input"
-        >
-          <b-form-input
-            id="bodyweight-input"
-            v-model="form.bodyweight"
-            type="number"
-            placeholder="Enter bodyweight here"
-            required
-          ></b-form-input>
-        </b-form-group>
-
-        <b-form-group id="bodyweight-units-group">
-          <b-form-checkbox-group
-            v-model="form.bodyweightUnits"
-            id="bodyweight-units-checkbox"
+          <b-form-group
+            id="bench-group"
+            label="Bench:"
+            label-for="bench-input"
           >
-            <b-form-checkbox value="lbs">lbs</b-form-checkbox>
-            <b-form-checkbox value="kgs">kgs</b-form-checkbox>
-          </b-form-checkbox-group>
-        </b-form-group>
+            <b-form-input
+              id="bench-input"
+              v-model="form.bench"
+              type="number"
+              placeholder="Enter bench press here"
+              required
+            ></b-form-input>
+          </b-form-group>
 
-        <b-button type="submit" variant="primary">Submit</b-button>
-      </b-form>
+          <b-form-group
+            id="deadlift-group"
+            label="Deadlift:"
+            label-for="deadlift-input"
+          >
+            <b-form-input
+              id="deadlift-input"
+              v-model="form.deadlift"
+              type="number"
+              placeholder="Enter deadlift here"
+              required
+            ></b-form-input>
+          </b-form-group>
 
-      <hr><br><br>
+          <b-form-group id="lift-units-group">
+            <b-form-checkbox-group
+              v-model="form.liftUnits"
+              id="lift-units-checkbox"
+            >
+              <b-form-checkbox value="lbs">lbs</b-form-checkbox>
+              <b-form-checkbox value="kgs">kgs</b-form-checkbox>
+            </b-form-checkbox-group>
+          </b-form-group>
 
-      <b-card class="mt-3" header="Form Data Result">
-        <pre class="m-0">{{ form }}</pre>
-    </b-card>
+          <b-form-group
+            id="age-group"
+            label="Age:"
+            label-for="age-input"
+          >
+            <b-form-input
+              id="age-input"
+              v-model="form.age"
+              type="number"
+              placeholder="Enter age here"
+              required
+            ></b-form-input>
+          </b-form-group>
+
+          <b-form-group
+            id="bodyweight-group"
+            label="Bodyweight:"
+            label-for="bodyweight-input"
+          >
+            <b-form-input
+              id="bodyweight-input"
+              v-model="form.bodyweight"
+              type="number"
+              placeholder="Enter bodyweight here"
+              required
+            ></b-form-input>
+          </b-form-group>
+
+          <b-form-group id="bodyweight-units-group">
+            <b-form-checkbox-group
+              v-model="form.bodyweightUnits"
+              id="bodyweight-units-checkbox"
+            >
+              <b-form-checkbox value="lbs">lbs</b-form-checkbox>
+              <b-form-checkbox value="kgs">kgs</b-form-checkbox>
+            </b-form-checkbox-group>
+          </b-form-group>
+
+          <b-button type="submit" variant="primary">Submit</b-button>
+        </b-form>
+
+        <hr><br><br>
+
+        <b-card class="mt-3" header="Form Data Result" v-if="show">
+          <pre class="m-0">{{ result }}</pre>
+      </b-card>
+      </div>
     </div>
-  </div>
+  </b-card>
 </template>
 
 <script>
-import axios from 'axios';
-
 export default {
-  name: 'HomePage',
   data() {
     return {
-      msg: '',
+      form: {
+        squat: 0,
+        bench: 0,
+        deadlift: 0,
+        liftUnits: 'lbs',
+        age: 0,
+        bodyweight: 0,
+        bodyweightUnits: 'lbs',
+      },
+      show: false,
+      result: null,
     };
   },
   methods: {
-    getMessage() {
-      const path = 'http://localhost:5000/ping';
-      axios.get(path)
-        .then((res) => {
-          this.msg = res.data;
-        })
-        .catch((error) => {
-          // eslint-disable-next-line
-          console.error(error);
-        });
-    },
     onSubmit(event) {
       event.preventDefault();
-      alert(JSON.stringify(this.form));
+      this.show = true;
+      this.result = JSON.stringify(this.form);
     },
-  },
-  created() {
-    this.getMessage();
   },
 };
 </script>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,11 +1,9 @@
-import Vue from 'vue';
-import BootstrapVue from 'bootstrap-vue';
+import BootstrapVue3 from 'bootstrap-vue-3';
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
+
 import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap-vue/dist/bootstrap-vue.css';
+import 'bootstrap-vue-3/dist/bootstrap-vue-3.css';
 
-Vue.use(BootstrapVue);
-
-createApp(App).use(router).mount('#app');
+createApp(App).use(router).use(BootstrapVue3).mount('#app');
