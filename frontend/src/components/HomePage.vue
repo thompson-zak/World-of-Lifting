7b269b0942dca45850134e92a4043c4222e4de8b
thompson-zak <zakthompson97@gmail.com>
2022-12-10 12:56:27 -0600

    Updated checkboxes to radios. Analyze lifts API calls implemented.

--- frontend/src/components/HomePage.vue
+++ frontend/src/components/HomePage.vue
@@ -7,7 +7,7 @@
 
         <hr><br><br>
 
-        <b-form @submit="onSubmit">
+        <b-form @submit="onSubmit" @reset="onReset">
           <b-form-group
             id="squat-group"
             label="Squat:"
@@ -16,7 +16,7 @@
             <b-form-input
               id="squat-input"
               v-model="form.squat"
-              type="number"
+              type="text"
               placeholder="Enter squat here"
               required
             ></b-form-input>
@@ -30,7 +30,7 @@
             <b-form-input
               id="bench-input"
               v-model="form.bench"
-              type="number"
+              type="text"
               placeholder="Enter bench press here"
               required
             ></b-form-input>
@@ -44,21 +44,21 @@
             <b-form-input
               id="deadlift-input"
               v-model="form.deadlift"
-              type="number"
+              type="text"
               placeholder="Enter deadlift here"
               required
             ></b-form-input>
           </b-form-group>
 
           <b-form-group id="lift-units-group">
-            <b-form-checkbox-group
-              v-model="form.liftUnits"
-              id="lift-units-checkbox"
-            >
-              <b-form-checkbox value="lbs">lbs</b-form-checkbox>
-              <b-form-checkbox value="kgs">kgs</b-form-checkbox>
-            </b-form-checkbox-group>
+            <b-form-radio-group
+              id="lift-units-radio"
+              v-model="selectedLiftUnits"
+              :options="liftOptions"
+            ></b-form-radio-group>
           </b-form-group>
+
+          <br>
 
           <b-form-group
             id="age-group"
@@ -68,7 +68,7 @@
             <b-form-input
               id="age-input"
               v-model="form.age"
-              type="number"
+              type="text"
               placeholder="Enter age here"
               required
             ></b-form-input>
@@ -82,23 +82,24 @@
             <b-form-input
               id="bodyweight-input"
               v-model="form.bodyweight"
-              type="number"
+              type="text"
               placeholder="Enter bodyweight here"
               required
             ></b-form-input>
           </b-form-group>
 
           <b-form-group id="bodyweight-units-group">
-            <b-form-checkbox-group
-              v-model="form.bodyweightUnits"
-              id="bodyweight-units-checkbox"
-            >
-              <b-form-checkbox value="lbs">lbs</b-form-checkbox>
-              <b-form-checkbox value="kgs">kgs</b-form-checkbox>
-            </b-form-checkbox-group>
+            <b-form-radio-group
+              id="bodyweight-units-radio"
+              v-model="selectedBodyweightUnits"
+              :options="bodyweightOptions"
+            ></b-form-radio-group>
           </b-form-group>
 
+          <br>
+
           <b-button type="submit" variant="primary">Submit</b-button>
+          <b-button type="reset" variant="danger">Reset</b-button>
         </b-form>
 
         <hr><br><br>
@@ -112,27 +113,65 @@
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
   data() {
     return {
       form: {
-        squat: 0,
-        bench: 0,
-        deadlift: 0,
+        squat: null,
+        bench: null,
+        deadlift: null,
         liftUnits: 'lbs',
-        age: 0,
-        bodyweight: 0,
+        age: null,
+        bodyweight: null,
         bodyweightUnits: 'lbs',
       },
       show: false,
       result: null,
+      selectedLiftUnits: 'lbs',
+      liftOptions: [
+        { text: 'Pounds', value: 'lbs' },
+        { text: 'Kilos', value: 'kgs' },
+      ],
+      selectedBodyweightUnits: 'lbs',
+      bodyweightOptions: [
+        { text: 'Pounds', value: 'lbs' },
+        { text: 'Kilos', value: 'kgs' },
+      ],
     };
   },
   methods: {
     onSubmit(event) {
       event.preventDefault();
+
+      const path = 'http://localhost:5000/analyze-lifts';
+      const requestBody = {
+        squat: Number(this.form.squat),
+        bench: Number(this.form.bench),
+        deadlift: Number(this.form.deadlift),
+        liftUnits: this.form.liftUnits,
+        age: Number(this.form.age),
+        bodyweight: Number(this.form.bodyweight),
+        bodyweightUnits: this.form.bodyweightUnits,
+      };
+      console.log(requestBody);
+      axios.post(path, requestBody)
+        .then((res) => {
+          this.result = res.data;
+        })
+        .catch((error) => {
+          // eslint-disable-next-line
+          console.error(error);
+        });
+
       this.show = true;
-      this.result = JSON.stringify(this.form);
+    },
+    onReset(event) {
+      event.preventDefault();
+      // Reset our form values
+      this.result = null;
+      this.show = false;
     },
   },
 };
